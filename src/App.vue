7b589962f396195562ec74cef64cd5d94c075a38
Sebastian gomez a <sebas970218@gmail.com>
2021-01-30 10:08:02 -0500

    update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar absolute color="blue lighten-1" dark app>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title class="text-h4">La tienda de Mercadolibre</v-toolbar-title>
+      </v-app-bar>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
---
+++ src/components/ItemCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card @click="details" class="ma-5 mx-auto" max-width="344" min-height="380">
+    <v-img :src="item.thumbnail" height="150px"></v-img>
+    <v-card-title>{{item.title}}</v-card-title>
+    <v-card-subtitle>{{item.address.state_name}}</v-card-subtitle>
+    <v-card-text class="text--primary">{{item.price}}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  props: ["item"],
+  methods: {
+    details() {
+      this.$router.push({ name: "Details", params: { itemId: this.item.id } });
+    },
+  },
+};
+</script>
